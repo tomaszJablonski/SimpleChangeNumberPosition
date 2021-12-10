9ad65f734f8d96734578c348bb8a01ca8ecb5ca4
MrMag1990 <MrMag1990@kursSDA.pl>
2021-12-10 21:41:11 +0100

    Simple

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	        int a = 10;
+            int b = 5;
+            int c = 25;
+        System.out.print(a + " " + b +" "+ c );
+        int temp1a=0;
+        int temp2b=0;
+        int temp3c=0;
+        temp1a=a;
+        temp2b=b;
+        temp3c=c;
+        System.out.println();
+        System.out.println(temp2b + " " + temp3c +" "+ temp1a);
+
+
+    }
+}
